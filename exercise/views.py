21f09ab97afcd2f2a83baff2582b5de45e8c4b7e
jhonatanmizu <natanjesuss20@gmail.com>
2025-06-18 20:49:56 -0300

    feat: add workout session and exercise category migrations

--- exercise/views.py
+++ exercise/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Exercise
+
+
+class ExerciseView(View):
+    template_name = "exercise/pages/index.html"
+    queryset = Exercise.objects.all()
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        exercise_id = request.GET.get("id")
+        exercise = self.queryset.filter(id=exercise_id)
+        context = {"exercise": exercise}
+        return render(request, self.template_name, context)
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        return render(request, self.template_name)
+
+    def put(self, request: HttpRequest) -> HttpResponse:
+        return render(request, self.template_name)
+
+    def delete(self, request: HttpRequest) -> HttpResponse:
+        return render(request, self.template_name)
--- workout/views.py
+++ workout/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class WorkoutView(View):
+    template_name = "workout/workout.html"
+    def __init__(self) -> None:
+        self.name = "WorkoutView"
+
+    def get(self) -> HttpResponse:
+        return render(self.request, self.template_name)
+
+    def post(self) -> HttpResponse:
+        return HttpResponse("POST request received")
+
+    def put(self) -> HttpResponse:
+        return HttpResponse("PUT request received")
+
+    def delete(self) -> HttpResponse:
+        return HttpResponse("DELETE request received")
+
+    def patch(self) -> HttpResponse:
+        return HttpResponse("PATCH request received")
